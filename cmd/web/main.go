c9ac5599e6a65ff572fe1d7c591b1ab36fdb75d8
KXLAA <kolade.afode@yahoo.com>
2022-12-09 07:59:23 +0000

    feat: complete chapter 3.5
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,30 +21,18 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	//Router
-	mux := http.NewServeMux()
-
 	// Initialize a new instance of application containing the dependencies.
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
 
-	//Handlers
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	//Static File server
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	//Necessary to use our error log when Go’s HTTP server
 	//encounters an error instead of the standard logger
 	server := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
