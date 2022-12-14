3598fe684fcbd26895e03a2b71aa236864fdeb28
KXLAA <kolade.afode@yahoo.com>
2022-12-14 06:52:43 +0000

    feat: complete chapters 4.1 - 4.3
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,12 +17,22 @@ type application struct {
 
 func main() {
 	//Command line flags
-	addr := flag.String("addr", ":4000", "HTTP network address") //Local Host address
+	addr := flag.String("addr", ":4000", "HTTP network address")                                   //Local Host address
+	dsn := flag.String("dsn", "web:password@/snippetbox?parseTime=true", "MySQL data source name") //data source name
 	flag.Parse()
 
 	//Custom loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
 
 	// Initialize a new instance of application containing the dependencies.
 	app := &application{
@@ -36,6 +49,20 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
